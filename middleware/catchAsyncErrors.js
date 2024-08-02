9cf12ac57ca040c161dbeb7aef3e70d9a218fd55
Abhishek Gogoi <abhisat495@gmail.com>
2024-08-02 23:46:51 +0530

    Added comments

--- middleware/catchAsyncErrors.js
+++ middleware/catchAsyncErrors.js
@@ -1,3 +1,9 @@
+// This module exports a higher-order function that catches errors in asynchronous route handlers and passes them to the Express error-handling middleware.
+
 module.exports = (theFunc) => (req, res, next) => {
+  // Promise.resolve ensures that theFunc is wrapped in a promise.
+  // This allows both synchronous and asynchronous functions to be handled.
+  // .catch(next) catches any errors that occur during the execution of theFunc
+  // and passes them to the next middleware, which is typically the error-handling middleware.
   Promise.resolve(theFunc(req, res, next)).catch(next);
 };
--- utils/errorHandler.js
+++ utils/errorHandler.js
@@ -1,8 +1,13 @@
+// Importing the built-in Error class in JavaScript to extend it.
 class ErrorHandler extends Error {
+  // Constructor method to initialize the ErrorHandler instance.
   constructor(message, statusCode) {
+    //super keyword calls the constructor of parent class which is "Error"
     super(message);
+    // Assign the provided statusCode to the ErrorHandler instance.
     this.statusCode = statusCode;
 
+    // Capture the stack trace, excluding the constructor call from it.
     Error.captureStackTrace(this, this.constructor);
   }
 }
